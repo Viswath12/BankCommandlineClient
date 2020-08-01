7a3ea305e0867c99e56236c253c4f55745fb4380
viswath <viswathv@gmail.com>
2020-08-01 23:45:10 +0800

    Renamed Package Enum to enums.

--- src/main/java/app/CommandCliMain.java
+++ src/main/java/app/CommandCliMain.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import Enum.CommandAction;
+import enums.CommandAction;
 import service.AccountService;
 
 /**
--- src/main/java/enums/CommandAction.java
+++ src/main/java/enums/CommandAction.java
@@ -1,4 +1,4 @@
-package Enum;
+package enums;
 
 import org.apache.commons.lang3.StringUtils;
 
